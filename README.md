34f40d6c51f946c304c4df86875939dbf2d81570
lizhaode <308697716@qq.com>
2017-07-17 18:49:34 +0800

    commit ahead go home

---
+++ README.md
@@ -0,0 +1,13 @@
+# ShadowRocketConfAuto
+
+此项目整合了 [gfwlist](https://github.com/gfwlist/gfwlist)和[Shadowrocket-ADBlock-Rules](https://github.com/h2y/Shadowrocket-ADBlock-Rules)的规则
+
+#### 为什么要做这个项目
+
+其实，[Shadowrocket-ADBlock-Rules](https://github.com/h2y/Shadowrocket-ADBlock-Rules)的规则基本够用。
+
+但是在日常使用中，发现有一些较为偏僻的网站， `Shadowrocket-ADBlock-Rules` 并没有收入规则中。
+
+而且 `Shadowrocket-ADBlock-Rules` 项目的ReadMe中已经说明， `ShadowRocket` 的作者已经表明，上千条规则的时间复杂度也是O(1)
+
+既然如此，不如直接整合所有的gfwlist项目中的网址，这样对于网站的适配性更优异。
--- parsePAC.py
+++ parsePAC.py
@@ -1,15 +1,21 @@
 import requests
 import base64
 import io
+import os
 
 
 class parsePAC():
-
-
+    '''
+    最终要生成一个文本文件，这个文件中包含所有代理规则，且规则的格式与ShadowRocket相符，即直接
+    '''
     def getPAC(self):
+        '''
+        在tmp目录下，生成一个干净的，只存在PAC规则地址的文件，为下一步处理做准备
+        '''
         gfwUrl = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
-        gfwFile = 'gfwlist.txt'
-        domainFile = 'domainFile.txt'
+        domainFile = 'tmp/allDomainFile.txt'
+        if not os.path.exists('tmp'):
+            os.mkdir('tmp')
 
         gfwBase = requests.get(gfwUrl).text
         gfwList = base64.b64decode(gfwBase).decode('UTF-8')
@@ -23,10 +29,31 @@ class parsePAC():
             if i.startswith('!') or i.startswith('['):
                 continue
             with open(domainFile, 'a') as domain:
+                # 去除空行
                 if not str.isspace(i):
                     domain.write(i)
 
+    def wipeGoogle(self, domainString):
+        '''
+        去除PAC文件中含有Google的规则
+        '''
+        if 'google' in domainString:
+            return None
+        return domainString
 
+    def addGoogle(self,file):
+        '''
+        在最终规则文件的末尾，增加一条匹配所有Google网站的规则
+        '''
+        with open(file,'a') as f:
+            f.write('DOMAIN-KEYWORD,google,Proxy')
+
+    def reutrnDomainList(self, file):
+        with open(file) as f:
+            for i in f.readlines():
+                domainString = self.wipeGoogle(i)
+                if not domainString:
+                    pass
 
 
 if __name__ == '__main__':
